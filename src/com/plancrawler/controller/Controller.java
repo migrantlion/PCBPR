e53119865610e6b163692dc3310c91896007cc66
MigrantLion <steve.soss@gmail.com>
2016-06-01 00:08:43 -0400

    tweak controller version to read 1.4.0

--- src/com/plancrawler/controller/Controller.java
+++ src/com/plancrawler/controller/Controller.java
@@ -23,7 +23,7 @@ import com.plancrawler.view.toolbars.RotToolbarEvent;
 
 public class Controller {
 
-	public final static String VERSION = "1.4.1";
+	public final static String VERSION = "1.4.0";
 
 	private Database db;
 	private DocumentHandler pdfDoc;
